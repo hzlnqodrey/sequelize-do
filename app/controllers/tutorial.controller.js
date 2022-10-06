838ddf9895e5b7f6f8d1d6e6e6ae66f70e596a98
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2022-10-06 18:18:01 +0700

    Update a Tutorial by the id in the request

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -41,7 +41,7 @@ exports.findAll = (req, res) => {
         .then(data => {
             res.send(data)
         })
-        .catch((error) => {
+        .catch(err => {
             res.status(500).send({
                 message: err.message || "Some error occurred while retrieving Tutorial."
             })
@@ -71,7 +71,27 @@ exports.findOne = (req, res) => {
 
 // Update a Tutorial by the id in the request
 exports.update = (req, res) => {
+    const id = req.params.id
 
+    Tutorial.update(req.body, {
+        where: { id: id }
+    })
+        .then(num => {
+            if ( num  == 1 ) {
+                res.send({
+                    message: "Tutorial was updated succesfully"
+                })
+            } else {
+                res.send({
+                    message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
+                })
+            }
+        })
+        .catch(err => {
+            res.send({
+                message: "Error updating Tutorial with id=" + id
+            })
+        })
 }
 
 // Delete a Tutorial with the specified id in the request
